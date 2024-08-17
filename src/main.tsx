90c031058f7c0c2628b0eae4089322921ac3b919
Aristidescosta <aristidiscosta200@gmail.com>
2024-08-17 19:12:36 +0100

    feat: configuração inicial das routas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { createRoot } from 'react-dom/client';
 import { StrictMode } from 'react';
 
 import { App } from './App';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: '/contact',
+        element: <h1>Contacto</h1>,
+      },
+      {
+        path: '/our-services',
+        element: <h1>Nossos serviços</h1>,
+      },
+      {
+        path: '/about-us',
+        element: <h1>Sobre nós</h1>,
+      },
+    ],
+    errorElement: <h1>Página de erro</h1>,
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
+  </StrictMode>
 );
